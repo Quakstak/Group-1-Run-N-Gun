1a56754e68cc4c946b6c9788883cf4a36a509144
IzzyBw0110 <isabelle12345090604@icloud.com>
2026-05-21 18:48:37 +0100

    changes

--- src/enemies/fire_worm.py
+++ src/enemies/fire_worm.py
@@ -28,11 +28,10 @@ class FireWormEnemy(Enemy):
         )
         attack_frames = slice_sprite_sheet_row(
             attack_sheet, row=0, frame_w=20, frame_h=31,
-            num_frames=8, stride_x=21, start_x=0, start_y=0
+            num_frames=8, stride_x=21, start_x=0, start_y=0,
             clamp= True
         )
 
-        self.state = "IDLE"
         self.idle_anim = Animation(idle_frames, frame_duration=0.25, loop = True)
         self.current_anim = self.idle_anim
 
@@ -41,10 +40,23 @@ class FireWormEnemy(Enemy):
         
         self.pos = pygame.Vector2(self.rect.topleft)
         self.vel = pygame.Vector2(0.0, 0.0)
+
+        self.health = 30
         self.on_ground = False
+        self.facing = 1
 
     def update(self, dt: float, level, player) -> None:
+
         self.vel.y += settings.GRAVITY * dt
+
+        self.pos.x += self.vel.x * dt
+        self.rect.x = round(self.pos.x)
+
+        if level.rect_collides_solid(self.rect):
+            self.pos.x -= self.vel.x * dt
+            self.rect.x = round(self.pos.x)
+            self.vel.x *= -1
+            self.facing *= -1
 
         self.pos.y += self.vel.y * dt
         self.rect.y = round(self.pos.y)
@@ -54,14 +66,27 @@ class FireWormEnemy(Enemy):
         for tile_rect in hits:
             if self.vel.y > 0:
                 self.rect.bottom = tile_rect.top
-                self.vel.y = 0
                 self.on_ground = True
             elif self.vel.y < 0:
                 self.rect.top = tile_rect.bottom
                 self.vel.y = 0
             self.pos.y = self.rect.y
 
+        if not self.on_ground:
+            probe = self.rect.move(0,1)
+            if level.get_solid_hits(probe):
+                self.on_ground = True
+        
         self.apply_anim(dt)
+
+
+
+
+
+
+
+
+
 
 
 

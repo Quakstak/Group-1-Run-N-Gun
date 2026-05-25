10d58f8f69392e6f9fcdaa10d42492535b91a90c
vltus <toby192837466@gmail.com>
2026-05-26 00:57:46 +0100

    Toby: Finished level2 with hitboxes and enemies. New level currently under level 1 so needs moving over.

---
+++ assets/levels/b&w.tsx
@@ -0,0 +1,122 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Props-01" tilewidth="16" tileheight="16" tilecount="112" columns="8">
+ <image source="Props-01.png" width="128" height="224"/>
+ <tile id="19">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="20">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="3">
+   <object id="2" x="7.54545" y="7.63636"/>
+  </objectgroup>
+ </tile>
+ <tile id="21">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="22">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="27">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="28">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="29">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="30">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="48">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="49">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="50">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="51">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="52">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+ </tile>
+ <tile id="56">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="57">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="58">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="59">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="60">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="64">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="65">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="66">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="67">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+ <tile id="68">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+ </tile>
+</tileset>
---
+++ assets/levels/b&w2.tsx
@@ -0,0 +1,96 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.12.1" name="Buildings" tilewidth="16" tileheight="16" tilecount="375" columns="25">
+ <image source="Buildings.png" width="400" height="240"/>
+ <tile id="89">
+  <properties>
+   <property name="solid" type="bool" value="false"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.63636" y="8"/>
+  </objectgroup>
+ </tile>
+ <tile id="182">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.36364" y="8.27273"/>
+  </objectgroup>
+ </tile>
+ <tile id="252">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="10" y="7.82895"/>
+  </objectgroup>
+ </tile>
+ <tile id="253">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.73684" y="7.76316"/>
+  </objectgroup>
+ </tile>
+ <tile id="254">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.81579" y="7.43421"/>
+  </objectgroup>
+ </tile>
+ <tile id="277">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.47368" y="8.55263"/>
+  </objectgroup>
+ </tile>
+ <tile id="278">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8.81579" y="8.81579"/>
+  </objectgroup>
+ </tile>
+ <tile id="279">
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="9.14474" y="6.84211"/>
+   <object id="2" x="9.07895" y="8.28947"/>
+  </objectgroup>
+ </tile>
+ <tile id="291">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.45455" y="8.18182"/>
+  </objectgroup>
+ </tile>
+ <tile id="292">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.72727" y="8.81818"/>
+  </objectgroup>
+ </tile>
+ <tile id="293">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.54545" y="8.45455"/>
+  </objectgroup>
+ </tile>
+ <tile id="316">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="8" y="8.09091"/>
+  </objectgroup>
+ </tile>
+ <tile id="317">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.36364" y="8.18182"/>
+  </objectgroup>
+ </tile>
+ <tile id="318">
+  <properties>
+   <property name="solid" type="bool" value="true"/>
+  </properties>
+  <objectgroup draworder="index" id="2">
+   <object id="1" x="7.27273" y="8.18182"/>
+  </objectgroup>
+ </tile>
+</tileset>

c1307ed796fb47dc1d533d873776e10e5b2f2b71
IzzyBw0110 <isabelle12345090604@icloud.com>
2026-05-25 16:04:24 +0100

    Comented my code

--- src/enemies/fire_worm.py
+++ src/enemies/fire_worm.py
@@ -7,7 +7,8 @@ from .. import settings
 from .enemy import Enemy
 
 class FireWormEnemy(Enemy):
-    
+    """Enemy that has a idle state an attack state and a death state
+    and follows the enemy when in the attack state."""
     
     def __init__(self, pos: tuple[int, int]):
         super().__init__()
@@ -16,6 +17,7 @@ class FireWormEnemy(Enemy):
         death_sheet = load_image("worm_death.png")
         attack_sheet = load_image("worm_attack.png")
         
+        """loading in the sprite sheets"""
 
         idle_frames = slice_sprite_sheet_row(
             idle_sheet, row=0, frame_w=90, frame_h=90,
@@ -32,6 +34,8 @@ class FireWormEnemy(Enemy):
             num_frames=16, stride_x=90, start_x=0, start_y=0,
             clamp=True
         )
+
+        """ slicing the sprite sheets to make then animate on screen."""
     
 
         self.state = "IDLE"
@@ -39,6 +43,8 @@ class FireWormEnemy(Enemy):
         self.death_anim = Animation(death_frames, frame_duration=0.2, loop=False)
         self.attack_anim = Animation(attack_frames, frame_duration=0.25, loop=False)
         self.current_anim = self.idle_anim
+
+        """ setting the starting animation to be idle on spawn"""
 
         self.image = self.current_anim.image
         self.rect = self.image.get_rect(topleft=pos)
@@ -49,9 +55,7 @@ class FireWormEnemy(Enemy):
         self.health = 30
         self.on_ground = False
         self.facing = 1
-
         self.base_speed = 20
-
         self.attack_range = 5
         self.attack_damage = 5
 
@@ -60,12 +64,15 @@ class FireWormEnemy(Enemy):
         self.current_anim = new_anim 
         if hasattr(self.current_anim, 'reset'):
             self.current_anim.reset()
+    """Allowing the enemy to change its animation state based on scenario in game"""
 
     def take_damage(self, amount: int) -> None:
         self.health -= amount
         if self.health <= 0 and self.state != "DIE":
             self.change_state("DIE", self.death_anim)
             self.vel.x = 0
+
+    """ Making the enemy not be invincible and also to do its dying state when its health runs out"""
     
     def update(self, dt: float, level, player) -> None:
         if self.state == "DIE":
@@ -76,7 +83,8 @@ class FireWormEnemy(Enemy):
                 self.kill()
 
             return
-        
+        """once the enemy is dead it disapears of the screen """
+
         dist_x = player.rect.centerx - self.rect.centerx
         abs_dist = abs(dist_x)
         y_dist = abs(player.rect.centery - self.rect.centery)
@@ -94,7 +102,7 @@ class FireWormEnemy(Enemy):
             self.image = self.current_anim.image
             
             return
-        
+        """ once the enemy kills the player it goes back to the idle animation"""
 
         if abs_dist <= self.attack_range and y_dist < 40:
             self.change_state("ATTACK", self.attack_anim)
@@ -102,6 +110,8 @@ class FireWormEnemy(Enemy):
         else:
             self.change_state("IDLE", self.idle_anim)
             self.vel.x = self.base_speed * self.facing
+        """ If the enemy is in the attack range of the player it goes to the attack 
+            state and if not then the enemy is in idle, enemy also faces player in attack state"""
 
         self.pos.x += self.vel.x * dt
         self.rect.x = int(self.pos.x)
@@ -111,6 +121,8 @@ class FireWormEnemy(Enemy):
 
         if self.facing == -1:
             self.image = pygame.transform.flip(self.image, True, False)
+
+        """ if the player goes behind the enemy the enemy turns to face the player"""
 
 
         
